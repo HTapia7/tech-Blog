3c0e4b812bab26b2c3500c68a46e60cb338d0aa0
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-02-28 11:20:25 -0800

    controllers, package json view and styling

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-// helper function
 const helpers = require('./utils/helpers');
 
-// handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
 
-// session (connects session to sequelize Database)
 const session = require('express-session');
 
 const app = express();
@@ -34,14 +31,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
